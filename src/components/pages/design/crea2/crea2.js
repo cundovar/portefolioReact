806b41181b68a08d60deb334b979f022295cc80c
Facundo VARAS <102789450+cundovar@users.noreply.github.com>
2023-10-04 17:17:22 +0200

    FINCREA2

--- src/components/pages/design/crea2/crea2.js
+++ src/components/pages/design/crea2/crea2.js
@@ -8,6 +8,7 @@ const Crea2 = () => {
 
    const [imagePrincipal, setImagePrincipal]=useState("/images/crea2/image-product-1.jpg")
    const [modalOpen,setModalOpen]=useState(false)
+   const [count, setCount] = useState(0);
    const changeImage=(imageNew)=>{
     setImagePrincipal(imageNew)
    }
@@ -18,23 +19,35 @@ const Crea2 = () => {
    const closeModal=()=>{
     setModalOpen(false)
    }
-  return (
-    <>
-      <div>
-        <section className="bg-stone-500 w-full p-10  flex items-center space-x-5 px-40 justify-center mb-96">
-          <div className="left w-1/2   bg-pink-200 flex flex-col   justify-center items-center">
-            <div className="image bg-pink-400 h-2/3 w-2/3 space-y-5 flex flex-col items-center justify-center">
-              <div className="w-full">
+   
+  
+
+    const incrementCount=()=>{
+        setCount(count +1)
+    }
+    const decrementCount=()=>{
+        if(count>0)
+        setCount(count-1)
+    }
+    
+    
+    return (
+        <>
+      <div className="max-sm:w-full">
+        <section className="bg-stone-100 max-sm:pb-10 p-10  w-full  max-sm:w-full  flex items-center space-x-5  justify-center mb-96 max-sm:flex-col">
+          <div className="left w-1/2 max-sm:w-full    flex flex-col   justify-center items-center">
+            <div className="image h-2/3 w-2/3  max-sm:w-full space-y-5 flex flex-col  items-center justify-center">
+              <div className="w-full cursor-pointer">
                 <img
                   src={imagePrincipal}
                   className=" w-full rounded-xl"
                   alt="product-principal"
                   onClick={openModal}
-                />
+                  />
               </div>
 
-              <div className="allSmallImage h-1/3 w-full bg-pink-300 space-x-5 flex  ">
-                <div className="Smallimage w-44 h-44 bg-stone-400">
+              <div className="allSmallImage h-1/3 w-full  space-x-5 flex  ">
+                <div className=" Smallimage w-44  ">
                   <img
                     src="/images/crea2/image-product-1.jpg"
                     className= {`w-full h-auto rounded-xl ${imagePrincipal==="/images/crea2/image-product-1.jpg" ? 'border-2 border-orange-700  opacity-50':''}` }
@@ -43,9 +56,9 @@ const Crea2 = () => {
                       changeImage("/images/crea2/image-product-1.jpg")
                     }
                     
-                  />
+                    />
                 </div>
-                <div className="Smallimage w-44 bg-stone-400">
+                <div className="Smallimage w-44 ">
                   <img
                     src="/images/crea2/image-product-2.jpg"
                     className= {`w-full h-auto rounded-xl ${imagePrincipal==="/images/crea2/image-product-2.jpg" ? 'border-2 border-orange-700  opacity-50':''}` }
@@ -56,7 +69,7 @@ const Crea2 = () => {
 
                   />
                 </div>
-                <div className="Smallimage w-44  bg-stone-400 ">
+                <div className="Smallimage w-44  ">
                   <img
                     src="/images/crea2/image-product-3.jpg"
                     alt="product-3"
@@ -64,9 +77,9 @@ const Crea2 = () => {
                         changeImage("/images/crea2/image-product-3.jpg")
                     }
                     className= {`w-full h-auto rounded-xl ${imagePrincipal==="/images/crea2/image-product-3.jpg" ? 'border-2 border-orange-700  opacity-50':''}` }
-                  />
+                    />
                 </div>
-                <div className="Smallimage w-44  bg-stone-400">
+                <div className="Smallimage w-44 ">
                   <img
                     src="/images/crea2/image-product-4.jpg"
                     className= {`w-full h-auto rounded-xl ${imagePrincipal==="/images/crea2/image-product-4.jpg" ? 'border-2 border-orange-700  opacity-50':''}` }
@@ -74,55 +87,63 @@ const Crea2 = () => {
                     onClick={()=>
                         changeImage("/images/crea2/image-product-4.jpg")
                       }
-                  />
+                      />
                 </div>
               </div>
             </div>
           </div>
-          <div className="right w-1/2  bg-pink-200 flex flex-col space-y-10 p-10  justify-center">
-            <div className="flex flex-col space-y-10 w-5/6">
+          <div className="right w-1/2  max-sm:w-full   flex flex-col space-y-10 p-10  justify-center">
+            <div className="flex flex-col space-y-10 w-full">
 
-            <div className="title">
+            <div className="title w-full ">
               <div>
-                <h2 className="text-orange-500">SNEAKER COMPAGNY</h2>
+                <h2 className="text-orange-500 lg:text-xl max-sm:text-center ">SNEAKER COMPAGNY</h2>
               </div>
-              <div>
-                <h1 className="text-7xl">Fall Limited Edition</h1>
-                <h1 className="text-7xl">Sneakers</h1>
+              <div className="mt-2">
+                <h1 className="2xl:text-7xl lg:text-5xl max-sm:text-4xl max-sm:text-stone-900  w-full">Edition Limité</h1>
+                <h1 className="2xl:text-7xl lg:text-5xl max-sm:text-4xl max-sm:text-stone-900 ">Sneakers</h1>
               </div>
             </div>
-            <div className="text w-4/6 text-justify">
-              loremqfs uierp ierhpguipzhp irzghiu pzhguiphzpiu irzghiupehgiz hp
-              gzh'ig gdfgdgddggd gdgdgdgdg dgdfgdgd gdgdgdgdg dgdgdgdgdg gdgd
-              dgdgdg gdgd hdh dhhh dhgdh hh rh hr rhrhrhr h r rhrhr
-              irzhgpzihgihziuh gizeiuhh ih gizh
+            <div >
+                <p className="text 2xl:w-4/6 text-justify text-stone-500" >
+                Vous trouvez ici une page produit avec selction d'iamges et un carouselle en modal en cliquant sur l'image principale. Une petit compteur pour le panier. 
+                </p>
+                <p className="text 2xl:w-4/6 text-justify text-stone-500">
+                    Cette page est codé 100% en Tailwind, responsive tablette, smartphone et ecran.
+                </p>
             </div>
-            <div className="flex items-center space-x-5">
+           
+            <div className="flex items-center space-x-5 max-sm: max-sm:justify-center ">
             <div className="price text-4xl"> 354£</div>
-            <div className="bg-orange-500 text-orange-900 p-1 rounded font-bold">50%</div>
+            <div className="bg-orange-500 text-orange-900 p-1 rounded font-bold">-50%</div>
 
             </div>
           
-            <div className="panier flex h-3.5 space-x-5 ">
-              <div className="cursor w-1/3 flex">
-                <div className="w-1/3 border border-stone-900 flex items-center justify-center p-4  ">+</div>
-                <div className="w-1/3 border border-stone-900 flex items-center justify-center p-4  ">0</div>
-                <div className="w-1/3 border border-stone-900 flex items-center justify-center p-4  ">-</div>
+            <div className="panier flex  space-x-1 w-2/3 max-sm:w-full justify-center items-center max-sm:flex-col max-sm:space-y-3 max-sm:pt-10 ">
+              <div className="cursor w-1/3 flex max-sm:w-full " >
+                <div className="  w-1/3 border rounded-xl bg-stone-300  flex items-center justify-center p-4 text-3xl max-sm:p-2 text-orange-600   " onClick={incrementCount} >+</div>
+                <div className="w-1/3 border  rounded-xl bg-stone-300 flex items-center justify-center p-4 text-3xl max-sm:p-2 "> {count} </div>
+                <div className="w-1/3 border rounded-xl bg-stone-300  flex items-center justify-center p-4 text-3xl max-sm:p-2  text-orange-600  " onClick={decrementCount} >-</div>
               </div>
-              <button className="flex items-center w-2/3 bg-pink-500 p-5">
-                <div>   <FontAwesomeIcon icon={faCartShopping} /></div>
-                <div>ajout au panier</div>
+              <button className="flex items-center justify-center rounded-2xl w-2/3 max-sm:w-full bg-orange-500 space-x-5 p-5 ">
+                <div >   <FontAwesomeIcon className="text-xl" icon={faCartShopping} /></div>
+                <div>Ajouter au panier</div>
               </button>
             </div>
             </div>
           </div>
         </section>
         {modalOpen && (
-            <Modal image={imagePrincipal} closeModal={closeModal}/>
+            <div className="gg flex items-center justify-center  ">
+
+                <Modal image={imagePrincipal} closeModal={closeModal}/>
+            </div>
         )}
       </div>
     </>
   );
-};
 
-export default Crea2;
+  
+}
+  export default Crea2;
+  
--- src/components/pages/design/crea2/modal.js
+++ src/components/pages/design/crea2/modal.js
@@ -1,23 +1,75 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import {  faAngleRight, faXmark } from "@fortawesome/free-solid-svg-icons";
+import Slider from "react-slick";
+import { useRef } from "react";
 
 
-const Modal=({image,closeModal} )=>{
+const Modal=({closeModal} )=>{
+
+   
+        const settings = {
+          dots: true,
+          infinite: true,
+          speed: 500,
+          slidesToShow: 1,
+          slidesToScroll: 1
+        };
+
+        const sliderRef = useRef();
+
+        const nextSlide = () => {
+          sliderRef.current.slickNext();
+        };
+      
+        const prevSlide = () => {
+          sliderRef.current.slickPrev();
+        };
 
     return(
-        <div className="modal absolute top-0 w-1/3 left-20">
-            <div className="modal-content">
-                <span className="close"
+      
+        <div className="modal w-1/3 relative  max-sm:w-full  mt-10 max-sm:mt-50  max-sm:-left:5">
+                <span className="close absolute -top-20 -right-7 p-5  "
                 onClick={closeModal}
                 >
                  
-                    <FontAwesomeIcon icon={faCartShopping} />
+                
+                    <FontAwesomeIcon className="text-4xl hover:bg-stone-300 transition-colors ease-in-out delay-150 hover:text-orange-500 p-2 rounded-full h-10 w-10" icon={faXmark} style={{color: "#ffffff",}}  />
 
                 </span>
-                <img src={image} alt=""/>
+                <div>
+            <div className="modal-content relative">
+                <Slider {...settings}  ref={sliderRef}>
+                   
+
+                <img className="rounded-2xl " src="/images/crea2/image-product-1.jpg"alt=""/>
+                    
+        
+
+                <img className="rounded-2xl " src="/images/crea2/image-product-2.jpg"alt=""/>
+                   
+
+                <img className="rounded-2xl " src="/images/crea2/image-product-3.jpg"alt=""/>
+                
+                   
+
+                <img className="rounded-2xl " src="/images/crea2/image-product-4.jpg"alt=""/>
+                 
+                </Slider>
+                </div>
+                <button className="absolute h-24 w-24 hover:border hover:border-stone-500 transition  ease-in-out delay-100   rounded-full bg-stone-100 flex items-center justify-center -left-0 -translate-x-2/4 -translate-y-2/4 top-1/2 hover:bg-stone-300   max-sm:hidden "
+                onClick={prevSlide}>
+                <FontAwesomeIcon className="text-5xl" icon={faAngleRight} rotation={180}  />
+                </button>
+                <button className="absolute  h-24 w-24 transition  ease-in-out delay-100  hover:border hover:border-stone-500 hover:bg-stone-300  rounded-full bg-stone-100 flex items-center justify-center -right-0 translate-x-2/4 -translate-y-2/4 top-1/2 max-sm:hidden"
+                onClick={nextSlide}>
+
+                <FontAwesomeIcon className="text-5xl" icon={faAngleRight}   />
+                </button>
+
             </div>
         </div>
+      
     )
 }
 
